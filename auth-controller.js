d5ef8a298df9791f77b5454910cd1dc6bf9e1ad2
gtopinio <mctopinio@up.edu.ph>
2023-03-11 02:27:38 +0800

    feature: add getCar endpoint

--- auth-controller.js
+++ auth-controller.js
@@ -1,4 +1,10 @@
 
+
+
 exports.helloWorld = (req, res) => {
+    res.send("Hello World!");
+}
+
+exports.getCar = (req, res) => {
     res.send("Hello World!");
 }
--- routes.js
+++ routes.js
@@ -3,4 +3,5 @@ const authController = require("./auth-controller");
 module.exports = (app) => {
 
     app.get("/hello-world", authController.helloWorld);
+    app.get("/get-car", authController.getCar);
 }

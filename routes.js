db0ecf5342365894416db877d377ed9d6a66002a
gtopinio <mctopinio@up.edu.ph>
2023-03-13 14:45:37 +0800

    new: initialize sample routes in routes.js

---
+++ routes.js
@@ -0,0 +1,7 @@
+const authController = require("./auth-controller");
+
+module.exports = (app, pool) => {
+
+    app.get("/hello-world", authController.helloWorld);
+    app.get("/get-users", authController.getUsers(pool));
+}

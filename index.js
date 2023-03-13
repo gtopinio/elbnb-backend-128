272c4cb239dbe8bec142a8b203edb386ccd08d08
gtopinio <mctopinio@up.edu.ph>
2023-03-13 14:44:44 +0800

    new: integrate express server and database connection in index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+// This file is for starting the actual backend server (CMSC 128 - E1L)
+
+// Dependencies
+const express = require("express");
+const mysql = require("mysql");
+
+// Port number : Use 3001 when testing locally
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Create a database connection pool [Temporary database]
+const pool = mysql.createPool({
+    host:"us-cdbr-east-06.cleardb.net",
+    user:"bb119cab8b99eb",
+    password:"b30902db",
+    database:"heroku_7b30b189342afea"
+  });
+
+// The two lines below is to ensure that the server has parser to read the body of incoming requests
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Pass the database connection pool to your routes module
+require("./routes")(app, pool);
+
+// start server
+app.listen(PORT, (err) => {
+    if(err){ console.log(err);}
+    else{console.log("Server listening at port " + PORT);}
+});

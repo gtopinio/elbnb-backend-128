bffe2148ae34c243e4207f2ba1a8fef784845d05
gtopinio <mctopinio@up.edu.ph>
2023-03-18 17:28:08 +0800

    update: allow CORS and update request.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mysql = require("mysql");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const appLink = "https://mockup-backend-128.herokuapp.com/"
 
 // Create a database connection pool
 const pool = mysql.createPool({
@@ -15,6 +16,15 @@ const pool = mysql.createPool({
 // The two lines below is to ensure that the server has parser to read the body of incoming requests
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+// allow CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", appLink);
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Access-Control-Allow-Methods, Origin, Accept, Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 // Pass the database connection pool to your routes module
 require("./routes")(app, pool);
--- request.js
+++ request.js
@@ -5,7 +5,7 @@ const needle = require('needle')
 // link to app
 const app = "https://mockup-backend-128.herokuapp.com/"
 
-needle.get(app + "/get-users",
+needle.post(app + "/get-users",
     (err,res) =>{
         if(err) console.log(err);
         else console.log(res.body);

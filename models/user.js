4d95fab7168e762286f3a0be51f556d760534425
gtopinio <mctopinio@up.edu.ph>
2023-03-18 23:00:54 +0800

    update: change sql query in user model, update requests.js

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcrypt");
 
 const User = {
   create: (connection, firstName, lastName, email, password, username, contactNo, callback) => {
-    const sql = 'INSERT INTO users (firstName, lastName, email, password, username, contact_no) VALUES (?, ?, ?, ?, ?, ?)';
+    const sql = 'INSERT INTO users (first_name, last_name, email, password, username, contact_no) VALUES (?, ?, ?, ?, ?, ?)';
     const hash = bcrypt.hashSync(password, 10); // 10 is the number of rounds for salting
     connection.query(sql, [firstName, lastName, email, hash, username, contactNo], (error, results) => {
       if (error) {
--- request.js
+++ request.js
@@ -5,19 +5,21 @@ const needle = require('needle')
 // link to app
 const app = "https://mockup-backend-128.herokuapp.com"
 
-needle.post(app + "/get-users",
-    (err,res) =>{
-        if(err) console.log(err);
-        else console.log(res.body);
-});
+// needle.post(app + "/get-users",
+//     (err,res) =>{
+//         if(err) console.log(err);
+//         else console.log(res.body);
+// });
 
-needle.post(app + "/signUp",
-    {
-    firstName: 'John',
-    lastName: 'Doe',
-    email: 'johndoe@example.com',
-    password: 'password123'
-    },
-    (err,res) =>{
-        console.log(res.body);
-});
+needle.post(app + "/signUp", {
+    first_name: "Genesis",
+    last_name: "Topinio",
+    email: "mgct@example.com",
+    password: "p@ssw0rd",
+    username: "mgct",
+    is_registered: 1,
+    is_admin: 1,
+    contact_no: "1234567890"
+  }, (err, res) => {
+    console.log(res.body);
+  });

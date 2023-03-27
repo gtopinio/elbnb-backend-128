c65797b6ba405399c8a56e60273f1893ba85109a
Lyco Lacuesta <lycosheen27@gmail.com>
2023-03-27 18:51:13 +0800

    fix: login endpoint undefined password
    - change user.password to user.user_password in auth-controller.js:57

--- index.js
+++ index.js
@@ -4,24 +4,43 @@ const url = require("url");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const appLink = "https://mockup-backend-128.herokuapp.com/"
+const appLink = "http://localhost:3000";
+// const appLink = "https://mockup-backend-128.herokuapp.com/"
 
 require('./models/user');
 
-// Parse the database URL from the config var
-const dbUrl = url.parse(process.env.CLEARDB_DATABASE_URL);
-console.log("URL: "+ dbUrl)
-if (!dbUrl) {
-  throw new Error('Database URL not found');
-}
+// // Parse the database URL from the config var
+// const dbUrl = url.parse(process.env.CLEARDB_DATABASE_URL);
+// console.log("URL: "+ dbUrl)
+// if (!dbUrl) {
+//   throw new Error('Database URL not found');
+// }
 
-// Create a connection pool to the database
+// // Create a connection pool to the database
+// const pool = mysql.createPool({
+//   host: dbUrl.hostname,
+//   user: dbUrl.auth.split(':')[0],
+//   password: dbUrl.auth.split(':')[1],
+//   database: dbUrl.pathname.substring(1),
+//   connectionLimit: 10,
+// });
+
+// pool.getConnection((err, connection) => {
+//   if (err) {
+//     console.log("Error connecting to database:", err);
+//   } else {
+//     console.log("Connected to database!");
+//     connection.release();
+//   }
+// });
+
+// TODO: This is for local testing, do not push to live
 const pool = mysql.createPool({
-  host: dbUrl.hostname,
-  user: dbUrl.auth.split(':')[0],
-  password: dbUrl.auth.split(':')[1],
-  database: dbUrl.pathname.substring(1),
-  connectionLimit: 10,
+  connectionLimit : 10,
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'localdb'
 });
 
 pool.getConnection((err, connection) => {
--- request.js
+++ request.js
@@ -3,23 +3,41 @@ const needle = require('needle')
 //================================================================
 
 // link to app
-const app = "https://mockup-backend-128.herokuapp.com"
+// const app = "https://mockup-backend-128.herokuapp.com"
+const app = "http://localhost:3001";
 
-// needle.post(app + "/get-users",
+// needle.post(app + "/signUp", {
+//   first_name: "Samantha",
+//   last_name: "Wong",
+//   email: "samantha.wong_1989@yahoo.com",
+//   password: "testPassword",
+//   contact_no: "95563864191",
+//   is_business_account: 1,
+//   is_admin: 0
+// }, (err, res) => {
+//   console.log(res.body);
+// });
+
+needle.post(app + "/login", {
+  email: "samantha.wong_1989@yahoo.com",
+  password: "testPassword"
+}, (err, res) => {
+  console.log(res.body);
+});
+
+// needle.post(app + "/signUp", {
+//   first_name: "Another",
+//   last_name: "User",
+//   email: "test@test.com",
+//   contact_no: "09123456789",
+//   is_business_account: 0,
+//   is_admin: 0
+// }, (err, res) => {
+//   console.log(res.body);
+// });
+
+// needle.get(app + "/get-users",
 //     (err,res) =>{
 //         if(err) console.log(err);
 //         else console.log(res.body);
 // });
-
-needle.post(app + "/signUp", {
-    first_name: "Genesis",
-    last_name: "Topinio",
-    email: "mgct@example.com",
-    password: "p@ssw0rd",
-    username: "mgct",
-    is_registered: 1,
-    is_admin: 1,
-    contact_no: "1234567890"
-  }, (err, res) => {
-    console.log(res.body);
-  });

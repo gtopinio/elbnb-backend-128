ccc5954f6cefb27cc58eff85db84ea5d289e24cb
gtopinio <mctopinio@up.edu.ph>
2023-04-06 22:11:52 +0800

    test: try different add accommodation structure inside the function

--- auth-controller.js
+++ auth-controller.js
@@ -121,21 +121,13 @@ exports.checkIfLoggedIn = (pool) => (req, res) => {
     });
 }
 
-exports.addAccommodation = (pool) => (req, res) => {
-  const { name, type, description, location, price, amenities } = req.body; // assuming amenities is an array of strings
-
-  console.log("Name: " + name);
-  console.log("Type: " + type);
-  console.log("Price: " + price);
-  console.log("Description: " + description);
-  console.log("Location: " + location);
-
+function checkAccommDup(name) {
   let hasDup = false;
 
-
-  // get the pool connection first
   pool.getConnection((err, connection) => {
-    if(err) return res.send({success: false});
+    if(err){
+      console.log("Error: " + err);
+    }
 
     else{
         // See if there's an accommodation with the same name
@@ -150,8 +142,115 @@ exports.addAccommodation = (pool) => (req, res) => {
     }
   });
 
+  return hasDup;
+}
 
-  return res.send({success: hasDup});
+exports.addAccommodation = (pool) => (req, res) => {
+  const { name, type, description, location, price, amenities } = req.body; // assuming amenities is an array of strings
+
+  console.log("Name: " + name);
+  console.log("Type: " + type);
+  console.log("Price: " + price);
+  console.log("Description: " + description);
+  console.log("Location: " + location);
+
+  // check if there's an accommodation that already has the same name
+  if(checkAccommDup(name)){
+    console.log("Duplicate accommodation.")
+    return res.send({success: false});
+  } else {
+    // get pool connection
+    pool.getConnection((err, connection) => {
+      if(err) {
+        console.log("Get Connection Error: " + err);
+        return res.send({success:false});
+      }
+        // begin transaction
+    connection.beginTransaction((err) => {
+      if(err){
+        console.log("Begin Transaction Error: " + err);
+        return res.send({success:false});
+      }
+
+      else{
+        // accommodation name doesn't exist, proceed with inserting the new accommodation
+        const accommodationQuery = `
+        INSERT INTO accommodations
+          (ACCOMMODATION_NAME, ACCOMMODATION_TYPE, ACCOMMODATION_DESCRIPTION, ACCOMMODATION_PRICE, ACCOMMODATION_LOCATION)
+        VALUES
+          (?, ?, ?, ?, ?)
+      `;
+      connection.query(accommodationQuery, [name, type, description, price, location], (err, resultQuery) => {
+                  if (err) {
+                    connection.rollback(() => {
+                      console.log("Insert Accommodation Error: " + err);
+                      res.send({success:false});
+                    });
+                  }
+                  else { // Successful insertion
+                    const accommodationId = resultQuery.insertId; // get the auto-generated id of the newly inserted accommodation
+
+                      if (amenities.length > 0) {
+                        // if there are amenities, insert them into the accommodation_amenities table
+                        const amenityQueries = amenities.map((amenity) => {
+                          return [`${accommodationId}-${amenity}`, accommodationId];
+                        });
+                        const amenityQuery = `
+                          INSERT INTO accommodation_amenities
+                            (ACCOMMODATION_AMENITIES_ID, ACCOMMODATION_ID)
+                          VALUES
+                            ?
+                        `;
+                        connection.query(amenityQuery, [amenityQueries], (err) => {
+                          if (err) {
+                            connection.rollback(() => {
+                              console.log("Query Amenities Error: " + err);
+                              res.send({success:false});
+                            });
+                          }
+
+                          // commit the transaction if everything is successful
+                          else connection.commit((err) => {
+                            if (err) {
+                              connection.rollback(() => {
+                                console.log("Insert Amenities Error: " + err);
+                              });
+                            }
+
+                            // return a JSON object indicating success
+                            else{
+                              return res.send({ success: true });
+                            }  
+                          });
+                        });
+                      } else {
+                        // commit the transaction if there are no amenities
+                        connection.commit((err) => {
+                          if (err) {
+                            connection.rollback(() => {
+                              console.log("Commit Error: " + err);
+                              res.send({success:false});
+                            });
+                          }
+
+                          // return a JSON object indicating success
+                          else{
+                            return res.send({ success: true });
+                          } 
+
+                        });
+                      }
+            
+                  }
+      });
+      }
+    });
+    
+  
+    });
+  }
+
+
 
   // pool.getConnection((err, connection) => {
   //   if (err){

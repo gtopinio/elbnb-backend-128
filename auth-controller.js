d4dc2daafd21377396f27f4abaa2eb33eb69ac90
gtopinio <mctopinio@up.edu.ph>
2023-04-11 16:50:21 +0800

    test: change variable instantiation for accommodation name

--- auth-controller.js
+++ auth-controller.js
@@ -254,7 +254,7 @@ exports.addAccommodation = (pool) => (req, res) => {
 };
 
 exports.getAccommodationIdByName = (pool) => (req, res) => {
-  const { name } = req.body.accommodationName;
+  const name = req.body.accommodationName;
   // print out name of listing
   console.log("Accommodation Name: "+ name);
   // get pool connection

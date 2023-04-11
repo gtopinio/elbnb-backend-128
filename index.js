1cd0e1e3d56a588a7259d9bcc541de226e370c5f
gtopinio <mctopinio@up.edu.ph>
2023-04-11 23:17:16 +0800

    fix: add new variable to instantiate multer

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mysql = require("mysql");
-const upload = require("multer");
+const multer = require("multer");
+const upload = multer();
 const url = require("url");
 
 const PORT = process.env.PORT || 3001;

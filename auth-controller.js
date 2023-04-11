ba5456738aa4a6ab516ca6ffa4de09c63d18fccc
gtopinio <mctopinio@up.edu.ph>
2023-04-11 22:25:04 +0800

    fix: add the import for mime-types in auth-controller

--- auth-controller.js
+++ auth-controller.js
@@ -1,6 +1,7 @@
 // Imports
 const jwt = require("jsonwebtoken");
 const cloudinary = require('cloudinary').v2;
+const mime = require('mime-types');
 const User = require('./models/user');
 
 // Configuration 

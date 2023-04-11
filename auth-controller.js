d91a4ab5e4757621c7c93b7c95b157c4ec2a1285
gtopinio <mctopinio@up.edu.ph>
2023-04-11 22:47:16 +0800

    test: commented back the else if clause in upload function

--- auth-controller.js
+++ auth-controller.js
@@ -397,7 +397,7 @@ exports.uploadAccommodationPic = (pool) => async (req, res) => {
   // Find the accommodation id from the request parameters
   const accommodationName = req.body.accommodationName;
 
-  console.log("Data: " + base64Data);
+  // console.log("Data: " + base64Data);
   console.log("Accommodation Name: " + accommodationName);
   
   // check if there's an accommodation that has the same name
@@ -407,29 +407,38 @@ exports.uploadAccommodationPic = (pool) => async (req, res) => {
       return res.send({ success: false });
     } else if (result > 0) {
 
-      pool.getConnection(async (err, connection) => {
-        if (err) {
-          console.log("Error: " + err);
-          callback(err, null);
-        } else {
-  
-        // Upload the image to Cloudinary
-        try {
-          const result = await cloudinary.uploader.upload(base64Data, { upload_preset: 'mockup_setup' });
-          const accommodationPictureId = result.public_id;
-          
-          // Update the accommodation_pictures table
-          const insertAccommodationPictureQuery = `INSERT INTO accommodation_pictures (ACCOMMODATION_PICTURE_ID, ACCOMMODATION_ID) VALUES ('${accommodationPictureId}', ${accommodationId})`;
-          await connection.query(insertAccommodationPictureQuery);
-          
-          // Return success response
-          return res.send({ success: true });
-        } catch (error) {
-          console.error(error);
-          return res.send({ success: false, message: 'Error uploading image' });
-        }
+      // Check if request contains an image
+      const contentType = req.headers['content-type'];
+      const isImage = contentType && mime.lookup(contentType).startsWith('image/');
+
+      if (!isImage) {
+        return res.send({ success: false, message: 'Request must contain an image' });
       }
-    });
+
+      return res.send({ success: true, id: result});
+    //   pool.getConnection(async (err, connection) => {
+    //     if (err) {
+    //       console.log("Error: " + err);
+    //       callback(err, null);
+    //     } else {
+  
+    //     // Upload the image to Cloudinary
+    //     try {
+    //       const result = await cloudinary.uploader.upload(base64Data, { upload_preset: 'mockup_setup' });
+    //       const accommodationPictureId = result.public_id;
+          
+    //       // Update the accommodation_pictures table
+    //       const insertAccommodationPictureQuery = `INSERT INTO accommodation_pictures (ACCOMMODATION_PICTURE_ID, ACCOMMODATION_ID) VALUES ('${accommodationPictureId}', ${accommodationId})`;
+    //       await connection.query(insertAccommodationPictureQuery);
+          
+    //       // Return success response
+    //       return res.send({ success: true });
+    //     } catch (error) {
+    //       console.error(error);
+    //       return res.send({ success: false, message: 'Error uploading image' });
+    //     }
+    //   }
+    // });
   } else {
     console.log("Full upload error");
     return res.send({ success: false });

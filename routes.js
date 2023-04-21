82d9d6d062d1f95c61c41ca21ae4f2d6f5bb185e
gtopinio <mctopinio@up.edu.ph>
2023-04-21 13:03:07 +0800

    update: remove test endpoint routes

--- routes.js
+++ routes.js
@@ -1,9 +1,6 @@
 const authController = require("./auth-controller");
 
 module.exports = (app, pool) => {
-
-    app.get("/hello-world", authController.helloWorld);
-    app.get("/get-users", authController.getUsers(pool));
     app.post("/signUp", authController.signUp(pool));
     app.post("/login", authController.login(pool));
     app.post("/checkIfLoggedIn", authController.checkIfLoggedIn(pool));

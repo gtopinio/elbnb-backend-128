6f395f7f985fe080e731b020383a7492c61650d0
gtopinio <mctopinio@up.edu.ph>
2023-05-01 00:39:26 +0800

    fix: change HTTP method of view all students from POST to GET

--- routes.js
+++ routes.js
@@ -30,5 +30,5 @@ module.exports = (app, pool) => {
     app.post("/accommodation/is-favorite", authController.isAccommodationFavorited(pool));
     app.post("/accommodation/get-reviews", authController.getAccommodationReviews(pool));
     app.post("/accommodation/get-ratings", authController.getAccommodationAverageRating(pool));
-    app.post("/view-all-students", authController.viewAllStudents(pool));
+    app.get("/view-all-students", authController.viewAllStudents(pool));
 }

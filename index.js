e4b1f51e073949f56cd5a8683853feb28a548c83
gtopinio <mctopinio@up.edu.ph>
2023-05-08 12:30:46 +0800

    test: disabled cors for testing

--- index.js
+++ index.js
@@ -45,13 +45,13 @@ app.use(cookieParser());
 app.use(upload.fields([{ name: 'accommodationName', maxCount: 1 }, { name: 'data', maxCount: 1 }, { name: 'username', maxCount: 1 }, { name: 'data', maxCount: 1 }]));
 
 // allow CORS
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", appLink);
-  res.setHeader("Access-Control-Allow-Methods", "POST");
-  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Access-Control-Allow-Methods, Origin, Accept, Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", appLink);
+//   res.setHeader("Access-Control-Allow-Methods", "POST");
+//   res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Access-Control-Allow-Methods, Origin, Accept, Content-Type");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
 
 // Pass the database connection pool to your routes module
 require("./routes")(app, pool);

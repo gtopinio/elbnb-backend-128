1e85993aea3ae3ebb3c6da5c51150e3b41ec7ae0
Lyco Lacuesta <lycosheen27@gmail.com>
2023-05-13 19:32:07 +0800

    feat: add route for getAccommodationsByOwner

--- routes.js
+++ routes.js
@@ -36,6 +36,7 @@ module.exports = (app, pool) => {
     app.post("/accommodation/get-accommodation-pic", accomodationController.getAccommodationPic(pool));
     app.post("/accommodation/remove-accommodation-pic", accomodationController.removeAccommodationPicture(pool));
     app.post("/accommodation/update-accommodation-pic", accomodationController.updateAccommodationPicture(pool));
+    app.post("/accommodation/get-user-accommodations", accomodationController.getAccommodationsByOwner(pool));
 
     // room management endpoints
     app.post("/accommodation/add-room", roomController.addNewRoom(pool));

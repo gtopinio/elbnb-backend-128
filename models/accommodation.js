83354cac960b5fe5fc4db0198a54d59143c21ce7
gtopinio <mctopinio@up.edu.ph>
2023-05-14 20:03:48 +0800

    update: remove filterRooms function and add try catch block again in accommodation model

--- models/accommodation.js
+++ models/accommodation.js
@@ -20,11 +20,11 @@ const Accommodation = {
         }
         });
     },
-
-    // This function takes a database connection pool, an accommodation name (unique), and a callback function as inputs. 
-    // It queries the database to retrieve the accommodation ID for the provided name and passes the result to the callback function. 
-    // If there is an error in the database query or connection, it logs the error and passes it to the callback function as the first parameter.
     getAccommodationIdByName: (pool, name, callback) => {
+            // This function takes a database connection pool, an accommodation name (unique), and a callback function as inputs. 
+            // It queries the database to retrieve the accommodation ID for the provided name and passes the result to the callback function. 
+            // If there is an error in the database query or connection, it logs the error and passes it to the callback function as the first parameter.
+
         pool.getConnection((err, connection) => {
         if (err) {
             console.log("Error: " + err);
@@ -35,41 +35,26 @@ const Accommodation = {
             if (err) {
                 console.log("Get Accomm Id Error: " + err);
                 callback(err, null);
-            } else if(typeof result[0].ACCOMMODATION_ID === "undefined") {
+            } else {
+                try{
+                if(typeof result[0].ACCOMMODATION_ID === "undefined") {
                     console.log("Get Accom Id: Undefined Object");
                     callback(null, 0);
-            }
-            else {
-                console.log("Get Accom Id: Defined Object");
-                callback(null, result[0].ACCOMMODATION_ID);
+                }
+                else {
+                    console.log("Get Accom Id: Defined Object");
+                    callback(null, result[0].ACCOMMODATION_ID);
+                }
+                } catch (err) {
+                console.log("Accommodation Not Found...");
+                callback(err, null);
+                }
+                
             }
             });
         }
         });
-    },
-
-    // The function takes in a database connection pool object and returns a callback function that filters a room based on the user's search criteria specified in the req.query object.
-    filterRooms: (pool, priceTo, priceFrom, capacity, callback) => {
-      const query = `
-        SELECT DISTINCT ACCOMMODATION_ID FROM room
-        WHERE 
-          (ROOM_PRICE <= ? OR ? IS NULL)
-          AND (ROOM_PRICE >= ? OR ? IS NULL)
-          AND (ROOM_CAPACITY = ? OR ? IS NULL)
-      `;
-      
-      pool.query(query, [priceTo, priceTo, priceFrom, priceFrom, capacity, capacity], (err, results) => {
-        if (err) {
-          callback(err, null);
-        } else {
-          const ids = results.map(result => result.ACCOMMODATION_ID);
-          callback(null, ids);
-        }
-      });
-    }
-    
-    
-  
+    },  
 }
 
 

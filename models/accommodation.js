9479ca818c3c9d32baed1d7d2f4710d67e6dbc55
gtopinio <mctopinio@up.edu.ph>
2023-05-14 20:37:49 +0800

    update: refactor getAccommodationIdByName function

--- models/accommodation.js
+++ models/accommodation.js
@@ -20,41 +20,30 @@ const Accommodation = {
         }
         });
     },
-    getAccommodationIdByName: (pool, name, callback) => {
-            // This function takes a database connection pool, an accommodation name (unique), and a callback function as inputs. 
-            // It queries the database to retrieve the accommodation ID for the provided name and passes the result to the callback function. 
-            // If there is an error in the database query or connection, it logs the error and passes it to the callback function as the first parameter.
-
-        pool.getConnection((err, connection) => {
-        if (err) {
-            console.log("Error: " + err);
-            callback(err, null);
-        } else {
-            const checkQuery = `SELECT ACCOMMODATION_ID FROM accommodation WHERE ACCOMMODATION_NAME = ?`;
-            connection.query(checkQuery, [name], (err, result) => {
+        getAccommodationIdByName: (pool, name, callback) => {
+            pool.getConnection((err, connection) => {
             if (err) {
-                console.log("Get Accomm Id Error: " + err);
+                console.log("Error: " + err);
                 callback(err, null);
             } else {
-                try{
-                if(typeof result[0].ACCOMMODATION_ID === "undefined") {
-                    console.log("Get Accom Id: Undefined Object");
+                const checkQuery = `SELECT ACCOMMODATION_ID FROM accommodation WHERE ACCOMMODATION_NAME = ?`;
+                connection.query(checkQuery, [name], (err, result) => {
+                if (err) {
+                    console.log("Get Accomm Id Error: " + err);
+                    callback(err, null);
+                } else {
+                    if (result.length === 0) {
+                    console.log("Accommodation Not Found...");
                     callback(null, 0);
-                }
-                else {
+                    } else {
                     console.log("Get Accom Id: Defined Object");
                     callback(null, result[0].ACCOMMODATION_ID);
+                    }
                 }
-                } catch (err) {
-                console.log("Accommodation Not Found...");
-                callback(err, null);
-                }
-                
+                });
             }
             });
-        }
-        });
-    },  
+        },
 }
 
 

ce66e24e7b59c07bc7e53456743649f191f64b8f
gtopinio <mctopinio@up.edu.ph>
2023-05-22 13:42:39 +0800

    test: fix the python script

--- connection_script.py
+++ connection_script.py
@@ -1,23 +1,30 @@
-import subprocess
+import requests
 import time
 
 def check_connection_limit():
-    process = subprocess.Popen(["heroku", "logs", "--tail"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, _ = process.communicate()
+    # Get Heroku logs using the API
+    response = requests.get("https://api.heroku.com/apps/mockup-backend-128/logs", 
+                            headers={"Accept": "application/vnd.heroku+json; version=3"})
 
-    if "Error: ER_USER_LIMIT_REACHED: User 'bb119cab8b99eb' has exceeded the 'max_user_connections'" in stdout.decode():
-        print("Exceeded max_user_connections. Scaling down web dynos...")
+    if response.status_code == 200:
+        logs = response.json()
+        error_message = "Error: ER_USER_LIMIT_REACHED: User 'bb119cab8b99eb' has exceeded the 'max_user_connections'"
 
-        # Scale down web dynos to 0
-        subprocess.run(["heroku", "ps:scale", "web=0"])
+        if any(log['message'] == error_message for log in logs):
+            print("Exceeded max_user_connections. Scaling down web dynos...")
 
-        # Wait for 60 seconds
-        time.sleep(60)
+            # Scale down web dynos to 0
+            subprocess.run(["heroku", "ps:scale", "web=0"])
 
-        print("Scaling up web dynos...")
-        # Scale up web dynos to 1
-        subprocess.run(["heroku", "ps:scale", "web=1"])
+            # Wait for 60 seconds
+            time.sleep(60)
+
+            print("Scaling up web dynos...")
+            # Scale up web dynos to 1
+            subprocess.run(["heroku", "ps:scale", "web=1"])
+        else:
+            print("No max_user_connections error detected.")
     else:
-        print("No max_user_connections error detected.")
+        print("Failed to retrieve Heroku logs.")
 
 check_connection_limit()

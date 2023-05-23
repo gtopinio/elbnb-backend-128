6338a97da57264e7d366215059c115e2b24239fe
gtopinio <mctopinio@up.edu.ph>
2023-05-23 15:57:55 +0800

    update: remove unnecessary script file

--- connection_script.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-def run_heroku_command(command):
-    subprocess.run(command, shell=True, check=True)
-
-# Example: Run a Heroku command to restart the app
-run_heroku_command('heroku logs --tail')

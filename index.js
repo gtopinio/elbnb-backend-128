3fc012fe44c95afa7b3f652efd117ead30b068b4
gtopinio <mctopinio@up.edu.ph>
2023-05-28 16:23:31 +0800

    update: add options method to cors

--- index.js
+++ index.js
@@ -42,7 +42,13 @@ app.use(cookieParser());
 app.use(upload.fields([{ name: 'accommodationName', maxCount: 1 }, { name: 'data', maxCount: 1 }, { name: 'username', maxCount: 1 }, { name: 'data', maxCount: 1 }]));
 
 // allow CORS
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Access-Control-Allow-Methods, Origin, Accept, Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 // Pass the database connection pool to your routes module
 require("./routes")(app, pool);

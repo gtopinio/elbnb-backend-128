c2f024a6fa0f38cb3ae19cfd5a79f42b654a39ee
gtopinio <mctopinio@up.edu.ph>
2023-06-03 22:38:14 +0800

    feature: add cron job to restart server every 10 minutes

--- index.js
+++ index.js
@@ -53,6 +53,34 @@ app.use((req, res, next) => {
 // Pass the database connection pool to your routes module
 require("./routes")(app, pool);
 
+// ========================== CRON JOB for RESTARTING THE SERVER ==========================
+
+const cron = require('node-cron');
+const HEROKU_APP_ID = process.env.HEROKU_APP_ID;
+const HEROKU_API_TOKEN = process.env.HEROKU_API_TOKEN;
+
+// Restart dynos every 10 minutes
+cron.schedule('*/10 * * * *', async () => {
+    try {
+        console.log("Restarting dynos every 10 minutes");
+        const fetch = await import('node-fetch').then((module) => module.default);
+        const response = await fetch(`https://api.heroku.com/apps/${HEROKU_APP_ID}/dynos`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/vnd.heroku+json; version=3',
+                'Authorization': `Bearer ${HEROKU_API_TOKEN}`
+            }
+        });
+        console.log("Response status:", response.status);
+        console.log("Response body:", await response.text());
+    } catch (error) {
+        console.log("Error:", error);
+    }
+});
+
+// ========================== END OF CRON JOB for RESTARTING THE SERVER ==========================
+
 // ================ START OF MESSAGING FEATURE ================
 // Chat functionalities
 const harperSaveMessage = require('./services/harper-save-message'); // For saving messages

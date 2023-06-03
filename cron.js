67ccdb73b08a3ba0b3308c4e53bfd60b116d9b9a
gtopinio <mctopinio@up.edu.ph>
2023-06-03 20:25:12 +0800

    fix: proper import for node-fetch

--- cron.js
+++ cron.js
@@ -1,17 +1,15 @@
 const cron = require('node-cron');
-const fetch = require('node-fetch');
 
-const HEROKU_APP_ID = process.env.HEROKU_APP_ID;
-const HEROKU_API_TOKEN = process.env.HEROKU_API_TOKEN;
 // Restart dynos every 20 seconds
 cron.schedule('*/20 * * * * *', async () => {
     console.log("Restarting dynos every 20 seconds...");
+    const fetch = await import('node-fetch').then((module) => module.default);
     const response = await fetch(`https://api.heroku.com/apps/${HEROKU_APP_ID}/dynos`, {
-    method: 'DELETE',
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/vnd.heroku+json; version=3',
-        'Authorization': `Bearer ${HEROKU_API_TOKEN}`
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/vnd.heroku+json; version=3',
+            'Authorization': `Bearer ${HEROKU_API_TOKEN}`
         }
     });
     console.log(response);

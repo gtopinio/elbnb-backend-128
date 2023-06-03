c45d310e790a11b9cd5b4b67462964017f32e0c3
gtopinio <mctopinio@up.edu.ph>
2023-06-03 22:28:30 +0800

    update: move cron job in index

--- index.js
+++ index.js
@@ -53,6 +53,30 @@ app.use((req, res, next) => {
 // Pass the database connection pool to your routes module
 require("./routes")(app, pool);
 
+const cron = require('node-cron');
+const HEROKU_APP_ID = process.env.HEROKU_APP_ID;
+const HEROKU_API_TOKEN = process.env.HEROKU_API_TOKEN;
+
+// Restart dynos every 20 seconds
+cron.schedule('*/20 * * * * *', async () => {
+    try {
+        console.log("Restarting dynos every 20 seconds");
+        const fetch = await import('node-fetch').then((module) => module.default);
+        const response = await fetch(`https://api.heroku.com/apps/${HEROKU_APP_ID}/dynos`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/vnd.heroku+json; version=3',
+                'Authorization': `Bearer ${HEROKU_API_TOKEN}`
+            }
+        });
+        console.log("Response status:", response.status);
+        console.log("Response body:", await response.text());
+    } catch (error) {
+        console.log("Error:", error);
+    }
+});
+
 // ================ START OF MESSAGING FEATURE ================
 // Chat functionalities
 const harperSaveMessage = require('./services/harper-save-message'); // For saving messages
@@ -164,28 +188,4 @@ io.on('connection', (socket) => {
 server.listen(PORT, (err) => {
     if(err){ console.log(err);}
     else{console.log("Server listening at port " + PORT);}
-});
-
-const cron = require('node-cron');
-const HEROKU_APP_ID = process.env.HEROKU_APP_ID;
-const HEROKU_API_TOKEN = process.env.HEROKU_API_TOKEN;
-
-// Restart dynos every 20 seconds
-cron.schedule('*/20 * * * * *', async () => {
-    try {
-        console.log("Restarting dynos every 20 seconds");
-        const fetch = await import('node-fetch').then((module) => module.default);
-        const response = await fetch(`https://api.heroku.com/apps/${HEROKU_APP_ID}/dynos`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/vnd.heroku+json; version=3',
-                'Authorization': `Bearer ${HEROKU_API_TOKEN}`
-            }
-        });
-        console.log("Response status:", response.status);
-        console.log("Response body:", await response.text());
-    } catch (error) {
-        console.log("Error:", error);
-    }
 });

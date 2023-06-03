593f05b010b4d65a3d8187b762dc9f618ae4a079
gtopinio <mctopinio@up.edu.ph>
2023-06-03 20:20:05 +0800

    feature: try to restart server dynos every 20 seconds using cron job

--- cron.js
+++ cron.js
@@ -1,6 +1,18 @@
 var cron = require('node-cron');
+import fetch from "node-fetch";
 
-// Print something every 20 seconds
-cron.schedule('*/20 * * * * *', () => {
-    console.log('running a task every 20 seconds');
+const HEROKU_APP_ID = process.env.HEROKU_APP_ID;
+const HEROKU_API_TOKEN = process.env.HEROKU_API_TOKEN;
+// Restart dynos every 20 seconds
+cron.schedule('*/20 * * * * *', async () => {
+    console.log("Restarting dynos every 20 seconds...");
+    const response = await fetch(`https://api.heroku.com/apps/${HEROKU_APP_ID}/dynos`, {
+    method: 'DELETE',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/vnd.heroku+json; version=3',
+        'Authorization': `Bearer ${HEROKU_API_TOKEN}`
+        }
+    });
+    console.log(response);
 });

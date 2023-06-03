ef434acad6a76c0be547482aeba290dd8702c392
gtopinio <mctopinio@up.edu.ph>
2023-06-03 20:56:29 +0800

    update: add verbose console logging for cron job

--- cron.js
+++ cron.js
@@ -2,15 +2,20 @@ const cron = require('node-cron');
 
 // Restart dynos every 20 seconds
 cron.schedule('*/20 * * * * *', async () => {
-    console.log("Restarting dynos every 20 seconds...");
-    const fetch = await import('node-fetch').then((module) => module.default);
-    const response = await fetch(`https://api.heroku.com/apps/${HEROKU_APP_ID}/dynos`, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/vnd.heroku+json; version=3',
-            'Authorization': `Bearer ${HEROKU_API_TOKEN}`
-        }
-    });
-    console.log(response);
+    try {
+        console.log("Restarting dynos every 20 seconds...");
+        const fetch = await import('node-fetch').then((module) => module.default);
+        const response = await fetch(`https://api.heroku.com/apps/${HEROKU_APP_ID}/dynos`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/vnd.heroku+json; version=3',
+                'Authorization': `Bearer ${HEROKU_API_TOKEN}`
+            }
+        });
+        console.log("Response status:", response.status);
+        console.log("Response body:", await response.text());
+    } catch (error) {
+        console.log("Error:", error);
+    }
 });

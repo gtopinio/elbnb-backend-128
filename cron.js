401db7677efd95c56cec670fb209b5ec77999a59
gtopinio <mctopinio@up.edu.ph>
2023-06-03 22:22:27 +0800

    update: update cron job to 20 seconds

--- cron.js
+++ cron.js
@@ -2,10 +2,10 @@ const cron = require('node-cron');
 const HEROKU_APP_ID = process.env.HEROKU_APP_ID;
 const HEROKU_API_TOKEN = process.env.HEROKU_API_TOKEN;
 
-// Restart dynos every 2 minutes
-cron.schedule('*/2 * * * *', async () => {
+// Restart dynos every 20 seconds
+cron.schedule('*/20 * * * * *', async () => {
     try {
-        console.log("Restarting dynos every 2 minutes...");
+        console.log("Restarting dynos every 20 seconds");
         const fetch = await import('node-fetch').then((module) => module.default);
         const response = await fetch(`https://api.heroku.com/apps/${HEROKU_APP_ID}/dynos`, {
             method: 'DELETE',

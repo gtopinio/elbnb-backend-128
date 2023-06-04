1c9ed0c95c1772627a510ce6277a370c8141b520
gtopinio <mctopinio@up.edu.ph>
2023-06-04 23:55:00 +0800

    test: try using cors using package for socket server

--- index.js
+++ index.js
@@ -42,13 +42,16 @@ app.use(cookieParser());
 app.use(upload.fields([{ name: 'accommodationName', maxCount: 1 }, { name: 'data', maxCount: 1 }, { name: 'username', maxCount: 1 }, { name: 'data', maxCount: 1 }]));
 
 // allow CORS
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET");
-  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Access-Control-Allow-Methods, Origin, Accept, Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
+
+app.use(cors()); // Add cors middleware
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "POST, GET");
+//   res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Access-Control-Allow-Methods, Origin, Accept, Content-Type");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
 
 
 // Pass the database connection pool to your routes module

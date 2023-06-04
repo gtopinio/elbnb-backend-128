ee51b07c07c60c830b6004352324edff9fef9997
gtopinio <mctopinio@up.edu.ph>
2023-06-04 11:01:15 +0800

    update: set cron job to 30 seconds

--- index.js
+++ index.js
@@ -60,10 +60,10 @@ const cron = require('node-cron');
 const HEROKU_APP_ID = process.env.HEROKU_APP_ID;
 const HEROKU_API_TOKEN = process.env.HEROKU_API_TOKEN;
 
-// Restart dynos every 20 seconds
-cron.schedule('*/20 * * * * *', async () => {
+// Restart dynos every 30 seconds
+cron.schedule('*/30 * * * * *', async () => {
     try {
-        console.log("Restarting dynos every 20 seconds");
+        console.log("Restarting dynos every 30 seconds");
         const fetch = await import('node-fetch').then((module) => module.default);
         const response = await fetch(`https://api.heroku.com/apps/${HEROKU_APP_ID}/dynos`, {
             method: 'DELETE',

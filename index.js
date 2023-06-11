ae2e3c31580eda01f5b159fa22a1c5abd2915306
gtopinio <mctopinio@up.edu.ph>
2023-06-11 15:46:41 +0800

    update: set cron job to 5 minutes

--- index.js
+++ index.js
@@ -63,10 +63,10 @@ const cron = require('node-cron');
 const HEROKU_APP_ID = process.env.HEROKU_APP_ID;
 const HEROKU_API_TOKEN = process.env.HEROKU_API_TOKEN;
 
-// Restart dynos every 1 minute
-cron.schedule('*/1 * * * *', async () => {
+// Restart dynos every 5 minutes
+cron.schedule('*/5 * * * *', async () => {
     try {
-        console.log("Restarting dynos every 1 minute...");
+        console.log("Restarting dynos every 5 minutes");
         const fetch = await import('node-fetch').then((module) => module.default);
         const response = await fetch(`https://api.heroku.com/apps/${HEROKU_APP_ID}/dynos`, {
             method: 'DELETE',
